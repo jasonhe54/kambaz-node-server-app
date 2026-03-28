90a779950b6c1271ffcadedec354bc5c8b91328d
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 16:55:40 -0400

    5.2.5.3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import hello from "./hello.js"
 import lab5 from "./lab5/index.js";
+import cors from "cors";
 
 const app = express()
+app.use(cors());
 lab5(app)
 hello(app)
 app.listen(process.env.PORT || 4000)
