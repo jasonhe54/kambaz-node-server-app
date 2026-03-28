720b94302149c018a1536d66960113c1fbedefd7
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 18:52:27 -0400

    5.4.3.2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from 'express'
+import session from "express-session";
 import hello from "./hello.js"
 import lab5 from "./lab5/index.js";
 import cors from "cors";
@@ -6,9 +8,27 @@ import db from "./kambaz/database/index.js";
 import UserRoutes from "./kambaz/users/routes.js";
 
 const app = express()
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+}));
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app, db);
 lab5(app)
 hello(app)
-app.listen(process.env.PORT || 4000)
+const port = process.env.PORT || 4000
+app.listen(port)
